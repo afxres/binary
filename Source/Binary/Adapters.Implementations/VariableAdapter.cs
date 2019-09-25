3790abe517921722f645eee5fc19c569b877ed67
miko <stdarg@outlook.com>
2019-09-25 20:08:28 +0800

    Fix performance issue for adapter

--- Source/Binary/Adapters.Implementations/VariableAdapter.cs
+++ Source/Binary/Adapters.Implementations/VariableAdapter.cs
@@ -20,31 +20,25 @@ namespace Mikodev.Binary.Adapters.Implementations
         public override ArraySegment<T> To(in ReadOnlySpan<byte> span)
         {
             Debug.Assert(converter.Length == 0);
-            var buffer = Array.Empty<T>();
+            var byteCount = span.Length;
+            if (byteCount == 0)
+                return new ArraySegment<T>(Array.Empty<T>());
+            const int InitialLength = 4;
+            var buffer = new T[InitialLength];
+            var limits = (long)InitialLength;
             var cursor = 0L;
-            var limits = 0L;
-
-            void Ensure()
-            {
-                Debug.Assert(cursor >= 0);
-                Debug.Assert(limits >= 0);
-                limits = limits == 0 ? 4 : limits * 2;
-                var target = new T[checked((int)limits)];
-                if (cursor != 0)
-                    Array.Copy(buffer, target, cursor);
-                buffer = target;
-            }
-
             var temp = span;
             while (!temp.IsEmpty)
             {
                 if (cursor >= limits)
-                    Ensure();
-                var item = converter.ToValueWithMark(ref temp);
-                buffer[cursor] = item;
-                cursor += 1;
+                {
+                    var target = new T[checked((int)(limits *= 2))];
+                    if (cursor != 0)
+                        Array.Copy(buffer, target, cursor);
+                    buffer = target;
+                }
+                buffer[cursor++] = converter.ToValueWithMark(ref temp);
             }
-
             Debug.Assert(cursor <= buffer.Length);
             return new ArraySegment<T>(buffer, 0, (int)cursor);
         }
