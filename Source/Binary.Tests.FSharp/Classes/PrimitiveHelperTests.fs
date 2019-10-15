0e9912e505131d8d60aaf240b2fe2c08ebf2a983
miko <stdarg@outlook.com>
2019-10-15 21:42:35 +0800

    Add helper class

---
+++ Source/Binary.Tests.FSharp/Classes/PrimitiveHelperTests.fs
@@ -0,0 +1,61 @@
+﻿module Classes.PrimitiveHelperTests
+
+open Mikodev.Binary
+open Xunit
+
+[<Fact>]
+let ``Encode From 0 To 63`` () =
+    let buffer = Array.zeroCreate<byte> 1
+    for i = 0 to 63 do
+        let mutable allocator = Allocator(buffer)
+        PrimitiveHelper.EncodeLengthPrefix(&allocator, uint32 i)
+        let span = allocator.AsSpan()
+        Assert.Equal(1, span.Length)
+        Assert.Equal(i, int span.[0])
+    ()
+
+[<Fact>]
+let ``Encode From 64 To 8191`` () =
+    let buffer = Array.zeroCreate<byte> 2
+    for i = 64 to 8191 do
+        let mutable allocator = Allocator(buffer)
+        PrimitiveHelper.EncodeLengthPrefix(&allocator, uint32 i)
+        let span = allocator.AsSpan()
+        Assert.Equal(2, span.Length)
+        Assert.Equal((i >>> 8) ||| 0x40, int span.[0])
+        Assert.Equal(byte i, span.[1])
+    ()
+
+[<Theory>]
+[<InlineData(0x0000_2000)>]
+[<InlineData(0x0000_8642)>]
+[<InlineData(0x00AB_CDEF)>]
+[<InlineData(0x7FFF_FFFF)>]
+let ``Encode More Than 8192`` (i : int) =
+    let buffer = Array.zeroCreate<byte> 4
+    let mutable allocator = Allocator(buffer)
+    PrimitiveHelper.EncodeLengthPrefix(&allocator, uint32 i)
+    let span = allocator.AsSpan()
+    Assert.Equal(4, span.Length)
+    Assert.Equal((i >>> 24) ||| 0x80, int span.[0])
+    Assert.Equal((i >>> 16) |> byte, span.[1])
+    Assert.Equal((i >>> 8) |> byte, span.[2])
+    Assert.Equal((i >>> 0) |> byte, span.[3])
+    ()
+
+[<Fact>]
+let ``Encode Then Decode From 0 To 16384`` () =
+    let buffer = Array.zeroCreate<byte> 4
+    for i = 0 to 16384 do
+        let mutable allocator = Allocator(buffer)
+        PrimitiveHelper.EncodeLengthPrefix(&allocator, uint32 i)
+        let span = allocator.AsSpan()
+        if i <= 0x3F then
+            Assert.Equal(1, span.Length)
+        elif i <= 0x1FFF then
+            Assert.Equal(2, span.Length)
+        else 
+            Assert.Equal(4, span.Length)
+        let result = PrimitiveHelper.DecodeLengthPrefix(&span)
+        Assert.Equal(i, result)
+    ()
---
+++ Source/Binary/PrimitiveHelper.cs
@@ -0,0 +1,90 @@
+﻿using Mikodev.Binary.Internal;
+using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Mikodev.Binary
+{
+    public static class PrimitiveHelper
+    {
+        /* x = 0 or 1
+         * 0b1xxx_xxxx fixed length 4 bytes
+         * 0b00xx_xxxx variable length 1 bytes
+         * 0b010x_xxxx variable length 2 bytes */
+
+        [DebuggerStepThrough, MethodImpl(MethodImplOptions.NoInlining)]
+        internal static void ThrowLengthPrefixOverflow() => throw new ArgumentException("Length prefix overflow!");
+
+        [DebuggerStepThrough, MethodImpl(MethodImplOptions.NoInlining)]
+        internal static int ThrowLengthPrefixInvalidBytes() => throw new ArgumentException("Length prefix bytes invalid!");
+
+        internal static void EncodeFixed4(ref byte bytes, uint item)
+        {
+            Memory.Add(ref bytes, 0) = (byte)((item >> 24) | 0x80);
+            Memory.Add(ref bytes, 1) = (byte)(item >> 16);
+            Memory.Add(ref bytes, 2) = (byte)(item >> 8);
+            Memory.Add(ref bytes, 3) = (byte)item;
+        }
+
+        public static void EncodeLengthPrefix(ref Allocator allocator, uint item)
+        {
+            if ((item & 0x8000_0000) != 0)
+            {
+                ThrowLengthPrefixOverflow();
+            }
+            else if ((item & 0xFFFF_FFC0) == 0)
+            {
+                ref var location = ref allocator.AllocateReference(1);
+                Memory.Add(ref location, 0) = (byte)item;
+            }
+            else if ((item & 0xFFFF_E000) == 0)
+            {
+                ref var location = ref allocator.AllocateReference(2);
+                Memory.Add(ref location, 0) = (byte)((item >> 8) | 0x40);
+                Memory.Add(ref location, 1) = (byte)item;
+            }
+            else
+            {
+                ref var location = ref allocator.AllocateReference(4);
+                EncodeFixed4(ref location, item);
+            }
+        }
+
+        public static int DecodeLengthPrefix(ref byte bytes, int byteCount)
+        {
+            var source = bytes;
+            if ((source & 0x80) != 0)
+            {
+                if (byteCount < 4)
+                    goto fail;
+                return ((source & 0x7F) << 24) | (Memory.Add(ref bytes, 1) << 16) | (Memory.Add(ref bytes, 2) << 8) | Memory.Add(ref bytes, 3);
+            }
+            if ((source & 0x40) == 0)
+            {
+                return source & 0x3F;
+            }
+            if ((source & 0x20) == 0)
+            {
+                if (byteCount < 2)
+                    goto fail;
+                return ((source & 0x1F) << 8) | Memory.Add(ref bytes, 1);
+            }
+
+        fail:
+            return ThrowLengthPrefixInvalidBytes();
+        }
+
+        public static int DecodeLengthPrefix(in ReadOnlySpan<byte> span)
+        {
+            var byteCount = span.Length;
+            if (byteCount == 0)
+                goto fail;
+            ref var bytes = ref MemoryMarshal.GetReference(span);
+            return DecodeLengthPrefix(ref bytes, byteCount);
+
+        fail:
+            return ThrowLengthPrefixInvalidBytes();
+        }
+    }
+}
