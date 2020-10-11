f91a697ce500198199e4875cf7e3e609d8a992f2
miko <stdarg@outlook.com>
2020-10-11 14:42:27 +0800

    Add collection enumeration benchmarks

---
+++ code/Benchmarks.EnumerationTests/EnumerationBenchmarks.cs
@@ -0,0 +1,76 @@
+﻿using BenchmarkDotNet.Attributes;
+using Mikodev.Binary.Benchmarks.EnumerationTests.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mikodev.Binary.Benchmarks.EnumerationTests
+{
+    [MemoryDiagnoser]
+    public class EnumerationBenchmarks
+    {
+        private byte[] buffer;
+
+        private Converter<int> converter;
+
+        private HashSet<int> collection;
+
+        private Dictionary<int, int> dictionary;
+
+        [Params(0, 1 << 4, 1 << 8, 1 << 12, 1 << 16, 1 << 20)]
+        public int Count;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            var source = Enumerable.Range(0, Count).ToList();
+            var generator = Generator.CreateDefault();
+            buffer = new byte[1 << 24];
+            converter = generator.GetConverter<int>();
+            collection = source.ToHashSet();
+            dictionary = source.ToDictionary(x => x);
+        }
+
+        [Benchmark(Description = "Encode HashSet (foreach interface)")]
+        public void S01()
+        {
+            var allocator = new Allocator(new Span<byte>(buffer));
+            EnumerationEncoder.EncodeEnumerableForEach(ref allocator, converter, collection);
+        }
+
+        [Benchmark(Description = "Encode HashSet (foreach)")]
+        public void S02()
+        {
+            var allocator = new Allocator(new Span<byte>(buffer));
+            EnumerationEncoder.EncodeHashSetForEach(ref allocator, converter, collection);
+        }
+
+        [Benchmark(Description = "Encode HashSet (to array then foreach)")]
+        public void S03()
+        {
+            var allocator = new Allocator(new Span<byte>(buffer));
+            EnumerationEncoder.EncodeCollectionToArrayThenForEach(ref allocator, converter, collection);
+        }
+
+        [Benchmark(Description = "Encode Dictionary (foreach interface)")]
+        public void D01()
+        {
+            var allocator = new Allocator(new Span<byte>(buffer));
+            EnumerationEncoder.EncodeKeyValueEnumerableForEach(ref allocator, converter, converter, dictionary);
+        }
+
+        [Benchmark(Description = "Encode Dictionary (foreach)")]
+        public void D02()
+        {
+            var allocator = new Allocator(new Span<byte>(buffer));
+            EnumerationEncoder.EncodeDictionaryForEach(ref allocator, converter, converter, dictionary);
+        }
+
+        [Benchmark(Description = "Encode Dictionary (to array then foreach)")]
+        public void D03()
+        {
+            var allocator = new Allocator(new Span<byte>(buffer));
+            EnumerationEncoder.EncodeKeyValueCollectionForEach(ref allocator, converter, converter, dictionary);
+        }
+    }
+}
---
+++ code/Benchmarks.EnumerationTests/Models/EnumerationEncoder.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+
+namespace Mikodev.Binary.Benchmarks.EnumerationTests.Models
+{
+    public static class EnumerationEncoder
+    {
+        public static void EncodeHashSetForEach<T>(ref Allocator allocator, Converter<T> converter, HashSet<T> collection)
+        {
+            if (collection is null)
+                return;
+            foreach (var i in collection)
+                converter.Encode(ref allocator, i);
+        }
+
+        public static void EncodeEnumerableForEach<T>(ref Allocator allocator, Converter<T> converter, IEnumerable<T> collection)
+        {
+            if (collection is null)
+                return;
+            foreach (var i in collection)
+                converter.Encode(ref allocator, i);
+        }
+
+        public static void EncodeCollectionToArrayThenForEach<T>(ref Allocator allocator, Converter<T> converter, ICollection<T> collection)
+        {
+            if (collection is null)
+                return;
+            var array = new T[collection.Count];
+            collection.CopyTo(array, 0);
+            foreach (var i in array)
+                converter.Encode(ref allocator, i);
+        }
+
+        public static void EncodeDictionaryForEach<K, V>(ref Allocator allocator, Converter<K> init, Converter<V> tail, Dictionary<K, V> collection)
+        {
+            if (collection is null)
+                return;
+            foreach (var i in collection)
+            {
+                init.Encode(ref allocator, i.Key);
+                tail.Encode(ref allocator, i.Value);
+            }
+        }
+
+        public static void EncodeKeyValueEnumerableForEach<K, V>(ref Allocator allocator, Converter<K> init, Converter<V> tail, IEnumerable<KeyValuePair<K, V>> collection)
+        {
+            if (collection is null)
+                return;
+            foreach (var i in collection)
+            {
+                init.Encode(ref allocator, i.Key);
+                tail.Encode(ref allocator, i.Value);
+            }
+        }
+
+        public static void EncodeKeyValueCollectionForEach<K, V>(ref Allocator allocator, Converter<K> init, Converter<V> tail, ICollection<KeyValuePair<K, V>> collection)
+        {
+            if (collection is null)
+                return;
+            var array = new KeyValuePair<K, V>[collection.Count];
+            collection.CopyTo(array, 0);
+            foreach (var i in array)
+            {
+                init.Encode(ref allocator, i.Key);
+                tail.Encode(ref allocator, i.Value);
+            }
+        }
+    }
+}
