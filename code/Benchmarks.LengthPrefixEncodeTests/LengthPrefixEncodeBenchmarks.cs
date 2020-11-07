90b360800d57b0763ad0272ef85e861cdd2bf45e
miko <stdarg@outlook.com>
2020-11-07 19:08:09 +0800

    Add benchmark project for length prefix encoding

---
+++ code/Benchmarks.LengthPrefixEncodeTests/LengthPrefixEncodeBenchmarks.cs
@@ -0,0 +1,37 @@
+﻿using BenchmarkDotNet.Attributes;
+using Mikodev.Binary.Benchmarks.LengthPrefixEncodeTests.Models;
+using System;
+
+namespace Mikodev.Binary.Benchmarks.LengthPrefixEncodeTests
+{
+    public class LengthPrefixEncodeBenchmarks
+    {
+        private byte[] buffer;
+
+        private Converter<int> converter;
+
+        [Params(1, 16, 32)]
+        public int Count;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            this.buffer = new byte[1024];
+            this.converter = new EmptyBytesConverter();
+        }
+
+        [Benchmark(Description = "Encode (reuse buffer)")]
+        public void E01()
+        {
+            var allocator = new Allocator(new Span<byte>(this.buffer));
+            this.converter.Encode(ref allocator, this.Count);
+        }
+
+        [Benchmark(Description = "Encode With Length Prefix (reuse buffer)")]
+        public void E02()
+        {
+            var allocator = new Allocator(new Span<byte>(this.buffer));
+            this.converter.EncodeWithLengthPrefix(ref allocator, this.Count);
+        }
+    }
+}
---
+++ code/Benchmarks.LengthPrefixEncodeTests/Models/EmptyBytesConverter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Mikodev.Binary.Benchmarks.LengthPrefixEncodeTests.Models
+{
+    public sealed class EmptyBytesConverter : Converter<int>
+    {
+        public override void Encode(ref Allocator allocator, int item) => AllocatorHelper.Expand(ref allocator, item);
+
+        public override int Decode(in ReadOnlySpan<byte> span) => throw new NotSupportedException();
+    }
+}
