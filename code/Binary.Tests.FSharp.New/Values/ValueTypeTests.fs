47d0104d5f5c05ebd0dafa32a24903325662737f
miko <stdarg@outlook.com>
2021-02-28 13:24:19 +0800

    Add basic unit test for half type

---
+++ code/Binary.Tests.FSharp.New/Values/ValueTypeTests.fs
@@ -0,0 +1,12 @@
+﻿module Values.ValueTypeTests
+
+open Mikodev.Binary
+open System
+open Xunit
+
+[<Fact>]
+let ``Half Converter`` () =
+    let generator = Generator.CreateDefault()
+    let converter = generator.GetConverter<Half>()
+    Assert.StartsWith("NativeEndianConverter`1", converter.GetType().Name)
+    ()
