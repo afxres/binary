ef6afbc4c846b19b48bd44bc405c45de5b73093c
miko <stdarg@outlook.com>
2021-03-15 22:27:20 +0800

    Add benchmark project for fsharp set

---
+++ code/Benchmarks.FSharpSetTests/Program.fs
@@ -0,0 +1,14 @@
+﻿namespace Mikodev.Binary.Benchmarks.FSharpSetTests
+
+open BenchmarkDotNet.Configs;
+open BenchmarkDotNet.Jobs;
+open BenchmarkDotNet.Running;
+open BenchmarkDotNet.Toolchains.InProcess.Emit;
+
+module Program =
+    [<EntryPoint>]
+    let Main _ =
+        let config = ManualConfig.Create(DefaultConfig.Instance)
+        config.AddJob(Job.ShortRun.WithToolchain(InProcessEmitToolchain.Instance)) |> ignore
+        BenchmarkRunner.Run<SetBenchmarks>(config) |> ignore
+        0
---
+++ code/Benchmarks.FSharpSetTests/SetBenchmarks.fs
@@ -0,0 +1,36 @@
+﻿namespace Mikodev.Binary.Benchmarks.FSharpSetTests
+
+open BenchmarkDotNet.Attributes
+open Mikodev.Binary
+open Mikodev.Binary.Benchmarks.Abstractions
+open System
+open System.Linq
+
+[<MemoryDiagnoser>]
+type SetBenchmarks() =
+    let mutable buffer : byte array = null
+
+    let mutable intSetConverter : Converter<Set<int>> = null
+
+    let mutable intSet = Set.empty<int>
+
+    [<Params(0, 1, 1024)>]
+    member val public Count = 0 with get, set
+
+    [<GlobalSetup>]
+    member me.Setup() =
+        buffer <- Array.zeroCreate 65536
+        let generator =
+            Generator.CreateDefaultBuilder()
+                .AddConverter(BinaryStringConverter())
+                .AddFSharpConverterCreators()
+                .Build()
+        intSetConverter <- generator.GetConverter<_>()
+        intSet <- Enumerable.Range(0, me.Count) |> Set
+        ()
+
+    [<Benchmark(Description = "Encode Set Of Int (converter)")>]
+    member __.SE01() =
+        let mutable allocator = Allocator(Span buffer)
+        intSetConverter.Encode(&allocator, intSet)
+        ()
