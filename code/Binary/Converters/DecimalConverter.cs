eaf729944260ef549cdd2ff2b923995636d54f1e
miko <stdarg@outlook.com>
2021-04-04 19:11:43 +0800

    Fix decimal converter

--- code/Binary/Converters/DecimalConverter.cs
+++ code/Binary/Converters/DecimalConverter.cs
@@ -10,22 +10,32 @@ namespace Mikodev.Binary.Converters
 
         public override void Encode(ref Allocator allocator, decimal item)
         {
-            ref var target = ref Allocator.Assign(ref allocator, sizeof(int) * 4);
-            ref var source = ref Unsafe.As<decimal, byte>(ref item);
-            LittleEndian.Encode(ref Unsafe.Add(ref target, sizeof(int) * 0), Unsafe.As<byte, int>(ref Unsafe.Add(ref source, sizeof(int) * 2)));
-            LittleEndian.Encode(ref Unsafe.Add(ref target, sizeof(int) * 1), Unsafe.As<byte, int>(ref Unsafe.Add(ref source, sizeof(int) * 3)));
-            LittleEndian.Encode(ref Unsafe.Add(ref target, sizeof(int) * 2), Unsafe.As<byte, int>(ref Unsafe.Add(ref source, sizeof(int) * 1)));
-            LittleEndian.Encode(ref Unsafe.Add(ref target, sizeof(int) * 3), Unsafe.As<byte, int>(ref Unsafe.Add(ref source, sizeof(int) * 0)));
+            const int Limits = 4;
+            ref var target = ref Allocator.Assign(ref allocator, sizeof(int) * Limits);
+#if NET5_0_OR_GREATER
+            var buffer = (stackalloc int[Limits]);
+            _ = decimal.GetBits(item, buffer);
+#else
+            var source = System.Runtime.InteropServices.MemoryMarshal.CreateReadOnlySpan(ref Unsafe.As<decimal, int>(ref item), Limits);
+            var buffer = (stackalloc int[Limits] { source[2], source[3], source[1], source[0] });
+#endif
+            for (var i = 0; i < Limits; i++)
+                LittleEndian.Encode(ref Unsafe.Add(ref target, sizeof(int) * i), buffer[i]);
+            return;
         }
 
         public override decimal Decode(in ReadOnlySpan<byte> span)
         {
+            const int Limits = 4;
             ref var source = ref MemoryHelper.EnsureLength(span, sizeof(int) * 4);
-            var alpha = LittleEndian.Decode<int>(ref Unsafe.Add(ref source, sizeof(int) * 0));
-            var bravo = LittleEndian.Decode<int>(ref Unsafe.Add(ref source, sizeof(int) * 1));
-            var delta = LittleEndian.Decode<int>(ref Unsafe.Add(ref source, sizeof(int) * 2));
-            var flags = LittleEndian.Decode<int>(ref Unsafe.Add(ref source, sizeof(int) * 3));
-            return new decimal(alpha, bravo, delta, ((uint)flags & 0x8000_0000) is not 0, (byte)(flags >> 16));
+            var buffer = (stackalloc int[Limits]);
+            for (var i = 0; i < Limits; i++)
+                buffer[i] = LittleEndian.Decode<int>(ref Unsafe.Add(ref source, sizeof(int) * i));
+#if NET5_0_OR_GREATER
+            return new decimal(buffer);
+#else
+            return new decimal(buffer[0], buffer[1], buffer[2], ((uint)buffer[3] & 0x8000_0000) is not 0, (byte)(buffer[3] >> 16));
+#endif
         }
     }
 }
