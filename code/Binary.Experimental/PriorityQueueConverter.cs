ab866305b05b13f955b23864d7b1dfea361c30b1
miko <stdarg@outlook.com>
2021-04-19 21:37:25 +0800

    Add support for priority queue

---
+++ code/Binary.Experimental/PriorityQueueConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mikodev.Binary.Experimental
+{
+    public sealed class PriorityQueueConverter<E, P> : Converter<PriorityQueue<E, P>>
+    {
+        private readonly Converter<E> init;
+
+        private readonly Converter<P> tail;
+
+        public PriorityQueueConverter(Converter<E> init, Converter<P> tail)
+        {
+            this.init = init;
+            this.tail = tail;
+        }
+
+        public override void Encode(ref Allocator allocator, PriorityQueue<E, P> item)
+        {
+            if (item is null)
+                return;
+            var init = this.init;
+            var tail = this.tail;
+            foreach (var (head, next) in item.UnorderedItems)
+            {
+                init.EncodeAuto(ref allocator, head);
+                tail.EncodeAuto(ref allocator, next);
+            }
+        }
+
+        public override PriorityQueue<E, P> Decode(in ReadOnlySpan<byte> span)
+        {
+            var item = new PriorityQueue<E, P>();
+            var init = this.init;
+            var tail = this.tail;
+            var body = span;
+            while (body.Length is not 0)
+            {
+                var head = init.DecodeAuto(ref body);
+                var next = tail.DecodeAuto(ref body);
+                item.Enqueue(head, next);
+            }
+            return item;
+        }
+    }
+}
---
+++ code/Binary.Experimental/PriorityQueueConverterCreator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mikodev.Binary.Experimental
+{
+    public sealed class PriorityQueueConverterCreator : IConverterCreator
+    {
+        public IConverter GetConverter(IGeneratorContext context, Type type)
+        {
+            if (type.IsGenericType is false || type.GetGenericTypeDefinition() != typeof(PriorityQueue<,>))
+                return null;
+            var arguments = type.GetGenericArguments();
+            var converters = arguments.Select(context.GetConverter).ToArray();
+            var converterArguments = converters.Cast<object>().ToArray();
+            var converterType = typeof(PriorityQueueConverter<,>).MakeGenericType(arguments);
+            var converter = Activator.CreateInstance(converterType, converterArguments);
+            return (IConverter)converter;
+        }
+    }
+}
