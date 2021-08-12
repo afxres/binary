33ad482010dc5fd28302bba8227819db66f5984b
miko <stdarg@outlook.com>
2021-08-12 23:24:20 +0800

    Fix priority queue converter creator

--- code/Binary/Creators/PriorityQueueConverterCreator.cs
+++ code/Binary/Creators/PriorityQueueConverterCreator.cs
@@ -1,20 +1,13 @@
 ﻿namespace Mikodev.Binary.Creators;
 
+using Mikodev.Binary.Internal;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 internal sealed class PriorityQueueConverterCreator : IConverterCreator
 {
     public IConverter GetConverter(IGeneratorContext context, Type type)
     {
-        if (type.IsGenericType is false || type.GetGenericTypeDefinition() != typeof(PriorityQueue<,>))
-            return null;
-        var arguments = type.GetGenericArguments();
-        var converters = arguments.Select(context.GetConverter).ToArray();
-        var converterArguments = converters.Cast<object>().ToArray();
-        var converterType = typeof(PriorityQueueConverter<,>).MakeGenericType(arguments);
-        var converter = Activator.CreateInstance(converterType, converterArguments);
-        return (IConverter)converter;
+        return CommonHelper.GetConverter(context, type, typeof(PriorityQueue<,>), typeof(PriorityQueueConverter<,>), null);
     }
 }
