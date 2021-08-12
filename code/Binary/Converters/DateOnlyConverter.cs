65e7656b13c5feff0c14fa40c42a08c59db6eb4c
miko <stdarg@outlook.com>
2021-08-12 23:16:44 +0800

    Fix date only and time only converters

--- code/Binary/Converters/DateOnlyConverter.cs
+++ code/Binary/Converters/DateOnlyConverter.cs
@@ -1,22 +1,19 @@
 ﻿namespace Mikodev.Binary.Converters;
 
+using Mikodev.Binary.Internal;
 using System;
-using System.Buffers;
-using System.Buffers.Binary;
 
 internal sealed class DateOnlyConverter : Converter<DateOnly>
 {
-    private static readonly SpanAction<byte, int> EncodeAction = BinaryPrimitives.WriteInt32LittleEndian;
-
     public DateOnlyConverter() : base(sizeof(int)) { }
 
     public override void Encode(ref Allocator allocator, DateOnly item)
     {
-        Allocator.Append(ref allocator, sizeof(int), item.DayNumber, EncodeAction);
+        LittleEndian.Encode(ref allocator, item.DayNumber);
     }
 
     public override DateOnly Decode(in ReadOnlySpan<byte> span)
     {
-        return DateOnly.FromDayNumber(BinaryPrimitives.ReadInt32LittleEndian(span));
+        return DateOnly.FromDayNumber(LittleEndian.Decode<int>(span));
     }
 }
--- code/Binary/Converters/TimeOnlyConverter.cs
+++ code/Binary/Converters/TimeOnlyConverter.cs
@@ -1,22 +1,19 @@
 ﻿namespace Mikodev.Binary.Converters;
 
+using Mikodev.Binary.Internal;
 using System;
-using System.Buffers;
-using System.Buffers.Binary;
 
 internal sealed class TimeOnlyConverter : Converter<TimeOnly>
 {
-    private static readonly SpanAction<byte, long> EncodeAction = BinaryPrimitives.WriteInt64LittleEndian;
-
     public TimeOnlyConverter() : base(sizeof(long)) { }
 
     public override void Encode(ref Allocator allocator, TimeOnly item)
     {
-        Allocator.Append(ref allocator, sizeof(long), item.Ticks, EncodeAction);
+        LittleEndian.Encode(ref allocator, item.Ticks);
     }
 
     public override TimeOnly Decode(in ReadOnlySpan<byte> span)
     {
-        return new TimeOnly(BinaryPrimitives.ReadInt64LittleEndian(span));
+        return new TimeOnly(LittleEndian.Decode<long>(span));
     }
 }
