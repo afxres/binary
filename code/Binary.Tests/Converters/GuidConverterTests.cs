62be81e072c8a5f4b88cf4eb196d7fd54cae4538
miko <stdarg@outlook.com>
2021-09-10 20:02:22 +0800

    Add unit test for guid converter

--- code/Binary.Tests/Converters/GuidConverterTests.cs
+++ code/Binary.Tests/Converters/GuidConverterTests.cs
@@ -14,4 +14,21 @@ public class GuidConverterTests
         Assert.Equal("Mikodev.Binary.Converters.GuidConverter", converter.GetType().FullName);
         Assert.Equal(Unsafe.SizeOf<Guid>(), converter.Length);
     }
+
+    [Theory(DisplayName = "Encode Decode")]
+    [InlineData("47a87bd4-053c-47c5-b970-7e2164b167bc")]
+    [InlineData("cca92bd5-e5af-4913-9f29-4d4ccfa77784")]
+    public void BasicTest(string data)
+    {
+        var guid = Guid.Parse(data);
+        var generator = Generator.CreateDefault();
+        var converter = generator.GetConverter<Guid>();
+        var buffer = converter.Encode(guid);
+        Assert.Equal(16, buffer.Length);
+        Assert.Equal(16, converter.Length);
+        var actual = new Guid(new ReadOnlySpan<byte>(buffer));
+        Assert.Equal(guid, actual);
+        var result = converter.Decode(buffer);
+        Assert.Equal(guid, result);
+    }
 }
