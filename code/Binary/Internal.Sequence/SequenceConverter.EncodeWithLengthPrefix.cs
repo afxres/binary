e795d8ae1a6fdcc39efa57d2537dbe13b487fac2
miko <stdarg@outlook.com>
2021-09-11 14:27:03 +0800

    Add nullable annotations for sequence converter

--- code/Binary/Internal.Sequence/SequenceConverter.EncodeWithLengthPrefix.cs
+++ code/Binary/Internal.Sequence/SequenceConverter.EncodeWithLengthPrefix.cs
@@ -2,7 +2,7 @@
 
 internal sealed partial class SequenceConverter<T>
 {
-    private void EncodeWithLengthPrefixInternal(ref Allocator allocator, T item)
+    private void EncodeWithLengthPrefixInternal(ref Allocator allocator, T? item)
     {
         var anchor = Allocator.Anchor(ref allocator, sizeof(int));
         this.encode.Invoke(ref allocator, item);
--- code/Binary/Internal.Sequence/SequenceConverter.cs
+++ code/Binary/Internal.Sequence/SequenceConverter.cs
@@ -9,17 +9,17 @@ internal sealed partial class SequenceConverter<T> : Converter<T>
 
     private readonly EncodeDelegate<T> encode;
 
-    public SequenceConverter(EncodeDelegate<T> encode, DecodePassSpanDelegate<T> decode)
+    public SequenceConverter(EncodeDelegate<T> encode, DecodePassSpanDelegate<T>? decode)
     {
         this.encode = encode;
         this.decode = decode ?? (_ => ThrowHelper.ThrowNoSuitableConstructor<T>());
     }
 
-    public override void Encode(ref Allocator allocator, T item) => this.encode.Invoke(ref allocator, item);
+    public override void Encode(ref Allocator allocator, T? item) => this.encode.Invoke(ref allocator, item);
 
-    public override void EncodeAuto(ref Allocator allocator, T item) => EncodeWithLengthPrefixInternal(ref allocator, item);
+    public override void EncodeAuto(ref Allocator allocator, T? item) => EncodeWithLengthPrefixInternal(ref allocator, item);
 
-    public override void EncodeWithLengthPrefix(ref Allocator allocator, T item) => EncodeWithLengthPrefixInternal(ref allocator, item);
+    public override void EncodeWithLengthPrefix(ref Allocator allocator, T? item) => EncodeWithLengthPrefixInternal(ref allocator, item);
 
     public override T Decode(in ReadOnlySpan<byte> span) => this.decode.Invoke(span);
 
