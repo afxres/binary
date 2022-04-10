4d769859c21e4cafd88b3aeb6288b2d19e8dcade
miko <stdarg@outlook.com>
2022-04-10 22:41:49 +0800

    Add benchmark project for allocator string methods

---
+++ code/Benchmarks.AllocatorStringTests/AllocatorStringBenchmarks.cs
@@ -0,0 +1,64 @@
+﻿namespace Mikodev.Binary.Benchmarks.AllocatorStringTests;
+
+using BenchmarkDotNet.Attributes;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+[MemoryDiagnoser]
+public class AllocatorStringBenchmarks
+{
+    [AllowNull]
+    private byte[] buffer;
+
+    [AllowNull]
+    private Encoding encoding;
+
+    [AllowNull]
+    private AllocatorWriter<string?> writer;
+
+    [AllowNull]
+    [Params("", "Hello, world!", "The quick brown fox jumps over the lazy dog.")]
+    public string Text;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        this.buffer = new byte[1024];
+        this.encoding = Encoding.UTF8;
+        this.writer = new AllocatorWriter<string?>((span, text) => this.encoding.GetBytes(text, span));
+    }
+
+    [Benchmark(Description = "Append")]
+    public void A01()
+    {
+        var allocator = new Allocator(this.buffer);
+        Allocator.Append(ref allocator, this.Text, this.encoding);
+    }
+
+    [Benchmark(Description = "Append With Length Prefix")]
+    public void A02()
+    {
+        var allocator = new Allocator(this.buffer);
+        Allocator.AppendWithLengthPrefix(ref allocator, this.Text, this.encoding);
+    }
+
+    [Benchmark(Description = "Writer")]
+    public void W01()
+    {
+        var allocator = new Allocator(this.buffer);
+        var text = this.Text;
+        var encoding = this.encoding;
+        var maxLength = encoding.GetMaxByteCount(text.Length);
+        Allocator.Append(ref allocator, maxLength, text, this.writer);
+    }
+
+    [Benchmark(Description = "Writer With Length Prefix")]
+    public void W02()
+    {
+        var allocator = new Allocator(this.buffer);
+        var text = this.Text;
+        var encoding = this.encoding;
+        var maxLength = encoding.GetMaxByteCount(text.Length);
+        Allocator.AppendWithLengthPrefix(ref allocator, maxLength, text, this.writer);
+    }
+}
---
+++ code/Benchmarks.AllocatorStringTests/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Mikodev.Binary.Benchmarks.AllocatorStringTests;
+
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.InProcess.Emit;
+
+internal class Program
+{
+    private static void Main()
+    {
+        var config = ManualConfig.Create(DefaultConfig.Instance);
+        _ = config.AddJob(Job.ShortRun.WithToolchain(InProcessEmitToolchain.Instance));
+        _ = BenchmarkRunner.Run<AllocatorStringBenchmarks>(config);
+    }
+}
