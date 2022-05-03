be9eb91a7143a3c013c7eec4e94c32f975ffb326
miko <stdarg@outlook.com>
2022-05-03 15:26:08 +0800

    Add raw converter span like adapter

---
+++ code/Binary/Features.Adapters/RawConverterSpanLikeAdapter.cs
@@ -0,0 +1,39 @@
+﻿namespace Mikodev.Binary.Features.Adapters;
+
+using Mikodev.Binary.Features.Contexts;
+using Mikodev.Binary.Internal;
+using Mikodev.Binary.Internal.Sequence;
+using Mikodev.Binary.Internal.SpanLike;
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+#if NET6_0_OR_GREATER
+[RequiresPreviewFeatures]
+internal sealed class RawConverterSpanLikeAdapter<T, U> : SpanLikeAdapter<T> where U : struct, IRawConverter<T>
+{
+    public override void Encode(ref Allocator allocator, ReadOnlySpan<T> item)
+    {
+        var length = U.Length;
+        ref var target = ref Allocator.Assign(ref allocator, checked(length * item.Length));
+        for (var i = 0; i < item.Length; i++)
+            U.Encode(ref Unsafe.Add(ref target, length * i), item[i]);
+        return;
+    }
+
+    public override MemoryBuffer<T> Decode(ReadOnlySpan<byte> span)
+    {
+        var limits = span.Length;
+        if (limits is 0)
+            return new MemoryBuffer<T>(Array.Empty<T>(), 0);
+        var length = U.Length;
+        var capacity = SequenceMethods.GetCapacity<T>(limits, length);
+        var result = new T[capacity];
+        ref var source = ref MemoryMarshal.GetReference(span);
+        for (var i = 0; i < capacity; i++)
+            result[i] = U.Decode(ref Unsafe.Add(ref source, length * i));
+        return new MemoryBuffer<T>(result, capacity);
+    }
+}
+#endif
--- code/Binary/Features/RawConverter.cs
+++ code/Binary/Features/RawConverter.cs
@@ -1,7 +1,12 @@
 ﻿namespace Mikodev.Binary.Features;
 
+using Mikodev.Binary.Converters.Endianness.Adapters;
+using Mikodev.Binary.Features.Adapters;
 using Mikodev.Binary.Features.Contexts;
+using Mikodev.Binary.Features.Instance;
 using Mikodev.Binary.Internal;
+using Mikodev.Binary.Internal.SpanLike;
+using Mikodev.Binary.Internal.SpanLike.Contexts;
 using System;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -9,7 +14,7 @@ using System.Runtime.Versioning;
 
 #if NET6_0_OR_GREATER
 [RequiresPreviewFeatures]
-internal sealed class RawConverter<T, U> : Converter<T> where U : struct, IRawConverter<T>
+internal sealed class RawConverter<T, U> : Converter<T>, ISpanLikeAdapterCreator<T> where T : unmanaged where U : struct, IRawConverter<T>
 {
     public RawConverter() : base(U.Length)
     {
@@ -38,29 +43,36 @@ internal sealed class RawConverter<T, U> : Converter<T> where U : struct, IRawCo
         return U.Decode(ref MemoryModule.EnsureLength(Converter.DecodeWithLengthPrefix(ref span), U.Length));
     }
 
-    public override byte[] Encode(T? item)
+    public override byte[] Encode(T item)
     {
         var result = new byte[U.Length];
         U.Encode(ref MemoryMarshal.GetReference(new Span<byte>(result)), item);
         return result;
     }
 
-    public override void Encode(ref Allocator allocator, T? item)
+    public override void Encode(ref Allocator allocator, T item)
     {
         U.Encode(ref Allocator.Assign(ref allocator, U.Length), item);
     }
 
-    public override void EncodeAuto(ref Allocator allocator, T? item)
+    public override void EncodeAuto(ref Allocator allocator, T item)
     {
         U.Encode(ref Allocator.Assign(ref allocator, U.Length), item);
     }
 
-    public override void EncodeWithLengthPrefix(ref Allocator allocator, T? item)
+    public override void EncodeWithLengthPrefix(ref Allocator allocator, T item)
     {
         var prefix = NumberModule.EncodeLength((uint)U.Length);
         ref var target = ref Allocator.Assign(ref allocator, U.Length + prefix);
         NumberModule.Encode(ref target, (uint)U.Length, prefix);
         U.Encode(ref Unsafe.Add(ref target, prefix), item);
+    }
+
+    public SpanLikeAdapter<T> GetAdapter()
+    {
+        if (typeof(U) == typeof(NativeEndianRawConverter<T>))
+            return new NativeEndianSpanLikeAdapter<T>();
+        return new RawConverterSpanLikeAdapter<T, U>();
     }
 }
 #endif
