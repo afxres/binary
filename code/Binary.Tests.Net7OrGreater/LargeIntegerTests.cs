9ed4af510fcec2bbfe4f37506db3abf9d4728c50
miko <stdarg@outlook.com>
2022-06-16 23:36:34 +0800

    Add support for int128 and uint128

---
+++ code/Binary.Tests.Net7OrGreater/LargeIntegerTests.cs
@@ -0,0 +1,67 @@
+﻿namespace Mikodev.Binary.Tests.Net7OrGreater;
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Numerics;
+using Xunit;
+
+public class LargeIntegerTests
+{
+    private static void BinaryIntegerBasicTest<T>(int size, T item) where T : IBinaryInteger<T>
+    {
+        var generator = Generator.CreateDefault();
+        var converter = generator.GetConverter<T>();
+        var converterType = converter.GetType();
+        Assert.Equal("RawConverter`2", converterType.Name);
+        var genericArguments = converterType.GetGenericArguments();
+        var rawConverterType = genericArguments.Last();
+        var standardName = $"{typeof(T).Name}RawConverter";
+        Assert.Equal(standardName, rawConverterType.Name);
+        Assert.Equal(size, converter.Length);
+        var buffer = converter.Encode(item);
+        var result = converter.Decode(buffer);
+        Assert.Equal(item, result);
+        var expectedBuffer = new byte[size];
+        var flag = item.TryWriteLittleEndian(expectedBuffer, out var bytesWritten);
+        Assert.True(flag);
+        Assert.Equal(size, bytesWritten);
+        Assert.Equal(expectedBuffer, buffer);
+    }
+
+    public static IEnumerable<object[]> Int128Data => new List<object[]>
+    {
+        new object[] { 16, default(Int128) },
+        new object[] { 16, Int128.MinValue },
+        new object[] { 16, Int128.MaxValue },
+        new object[] { 16, Int128.Zero },
+        new object[] { 16, Int128.One },
+        new object[] { 16, Int128.NegativeOne },
+        new object[] { 16, Int128.Parse("11223344_55667788_99AABBCC_DDEEFF00".Replace("_", string.Empty), NumberStyles.HexNumber) },
+    };
+
+    [Theory(DisplayName = "Int128 Converter Basic Info")]
+    [MemberData(nameof(Int128Data))]
+    public void Int128EncodeDecode(int size, Int128 item)
+    {
+        BinaryIntegerBasicTest(size, item);
+    }
+
+    public static IEnumerable<object[]> UInt128Data => new List<object[]>
+    {
+        new object[] { 16, default(UInt128) },
+        new object[] { 16, UInt128.MinValue },
+        new object[] { 16, UInt128.MaxValue },
+        new object[] { 16, UInt128.Zero },
+        new object[] { 16, UInt128.One },
+        new object[] { 16, UInt128.Parse("11223344_55667788_99AABBCC_DDEEFF00".Replace("_", string.Empty), NumberStyles.HexNumber) },
+    };
+
+    [Theory(DisplayName = "UInt128 Converter Basic Info")]
+    [MemberData(nameof(UInt128Data))]
+    public void UInt128EncodeDecode(int size, UInt128 item)
+    {
+        BinaryIntegerBasicTest(size, item);
+    }
+}
---
+++ code/Binary/Features.Instance/Int128RawConverter.cs
@@ -0,0 +1,27 @@
+﻿namespace Mikodev.Binary.Features.Instance;
+
+using System;
+using System.Buffers.Binary;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#if NET7_0_OR_GREATER
+internal readonly struct Int128RawConverter : IRawConverter<Int128>
+{
+    public static int Length => Unsafe.SizeOf<Int128>();
+
+    public static Int128 Decode(ref byte source)
+    {
+        var lower = BinaryPrimitives.ReadUInt64LittleEndian(MemoryMarshal.CreateReadOnlySpan(ref Unsafe.Add(ref source, 0), sizeof(ulong)));
+        var upper = BinaryPrimitives.ReadUInt64LittleEndian(MemoryMarshal.CreateReadOnlySpan(ref Unsafe.Add(ref source, 8), sizeof(ulong)));
+        return new Int128(upper, lower);
+    }
+
+    public static void Encode(ref byte target, Int128 item)
+    {
+        ref var source = ref Unsafe.As<Int128, ulong>(ref item);
+        BinaryPrimitives.WriteUInt64LittleEndian(MemoryMarshal.CreateSpan(ref Unsafe.Add(ref target, 0), sizeof(ulong)), Unsafe.Add(ref source, 0));
+        BinaryPrimitives.WriteUInt64LittleEndian(MemoryMarshal.CreateSpan(ref Unsafe.Add(ref target, 8), sizeof(ulong)), Unsafe.Add(ref source, 1));
+    }
+}
+#endif
