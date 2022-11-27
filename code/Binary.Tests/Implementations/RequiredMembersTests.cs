77d8ddecea875a803ba245574ea5b396ea2dc396
miko <stdarg@outlook.com>
2022-11-27 19:20:54 +0800

    Add unit tests for required members

--- code/Binary.Tests/Implementations/RequiredMembersTests.cs
+++ code/Binary.Tests/Implementations/RequiredMembersTests.cs
@@ -1,5 +1,7 @@
 ﻿namespace Mikodev.Binary.Tests.Implementations;
 
+using Mikodev.Binary.Attributes;
+using System;
 using System.Linq;
 using Xunit;
 
@@ -49,5 +51,81 @@ public class RequiredMembersTests
         Assert.Null(rc.Name);
         Assert.Equal(6, rd.Id);
         Assert.Equal("Six", rd.Name);
+    }
+
+    [NamedObject]
+    private class MissingNamedKeyOnRequiredObject
+    {
+        public required int Id { get; init; }
+    }
+
+    [TupleObject]
+    private class MissingTupleKeyOnRequiredObject
+    {
+        public required long Flag { get; init; }
+    }
+
+    [Fact(DisplayName = "Missing Named Key For Required Member")]
+    public void MissingNamedKey()
+    {
+        var generator = Generator.CreateDefault();
+        var error = Assert.Throws<ArgumentException>(() => generator.GetConverter<MissingNamedKeyOnRequiredObject>());
+        var message = $"Require 'NamedKeyAttribute' for required member, property name: Id, type: {typeof(MissingNamedKeyOnRequiredObject)}";
+        Assert.Equal(message, error.Message);
+    }
+
+    [Fact(DisplayName = "Missing Tuple Key For Required Member")]
+    public void MissingTupleKey()
+    {
+        var generator = Generator.CreateDefault();
+        var error = Assert.Throws<ArgumentException>(() => generator.GetConverter<MissingTupleKeyOnRequiredObject>());
+        var message = $"Require 'TupleKeyAttribute' for required member, property name: Flag, type: {typeof(MissingTupleKeyOnRequiredObject)}";
+        Assert.Equal(message, error.Message);
+    }
+
+    private class DataType
+    {
+        public required int RequiredId { get; init; }
+
+        public string? OptionalName { get; init; }
+
+        public Guid OptionalGuid { get; init; }
+
+        public required string RequiredMessage { get; init; }
+    }
+
+    [Fact(DisplayName = "Decode Missing Optional Member")]
+    public void DecodeMissingOptionalMember()
+    {
+        var generator = Generator.CreateDefault();
+        var converter = generator.GetConverter<DataType>();
+        var id = Guid.Parse("1a57a6fd-61cd-4faf-81b5-cb97bb8fa5dd");
+        var a = converter.Decode(generator.Encode(new { RequiredId = 1024, RequiredMessage = "Hello", OptionalGuid = id }));
+        Assert.Equal(1024, a.RequiredId);
+        Assert.Equal("Hello", a.RequiredMessage);
+        Assert.Null(a.OptionalName);
+        Assert.Equal(id, a.OptionalGuid);
+
+        var b = converter.Decode(generator.Encode(new { OptionalName = "Bravo", RequiredMessage = "Nice", RequiredId = 65535 }));
+        Assert.Equal(65535, b.RequiredId);
+        Assert.Equal("Bravo", b.OptionalName);
+        Assert.Equal("Nice", b.RequiredMessage);
+        Assert.Equal(default, b.OptionalGuid);
+    }
+
+    [Fact(DisplayName = "Decode Missing Required Member")]
+    public void DecodeMissingRequiredMember()
+    {
+        var generator = Generator.CreateDefault();
+        var converter = generator.GetConverter<DataType>();
+        var a = Assert.Throws<ArgumentException>(() => _ = converter.Decode(generator.Encode(new { RequiredMessage = "0.0" })));
+        var m = $"Named key 'RequiredId' does not exist, type: {typeof(DataType)}";
+        Assert.Null(a.ParamName);
+        Assert.Equal(m, a.Message);
+
+        var b = Assert.Throws<ArgumentException>(() => _ = converter.Decode(generator.Encode(new { RequiredId = 768 })));
+        var n = $"Named key 'RequiredMessage' does not exist, type: {typeof(DataType)}";
+        Assert.Null(b.ParamName);
+        Assert.Equal(n, b.Message);
     }
 }
