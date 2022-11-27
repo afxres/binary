192099121b63b35e1c51247f432a4a5fd4a8eaa5
miko <stdarg@outlook.com>
2022-11-27 20:15:48 +0800

    Remove unused codes

--- code/Binary/Internal/LittleEndian.cs
+++ code/Binary/Internal/LittleEndian.cs
@@ -16,16 +16,4 @@ internal static class LittleEndian
     {
         return MemoryModule.EnsureEndian(Unsafe.ReadUnaligned<T>(ref location), BitConverter.IsLittleEndian is false);
     }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    internal static void Encode<T>(ref Allocator allocator, T item) where T : unmanaged
-    {
-        Encode(ref Allocator.Assign(ref allocator, Unsafe.SizeOf<T>()), item);
-    }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    internal static T Decode<T>(ReadOnlySpan<byte> span) where T : unmanaged
-    {
-        return Decode<T>(ref MemoryModule.EnsureLength(span, Unsafe.SizeOf<T>()));
-    }
 }
