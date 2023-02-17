5e22a789f1f6c12d2a47f028c6d67a1e17c82598
miko <stdarg@outlook.com>
2023-02-17 23:57:33 +0800

    Add experimental converter for bit array

---
+++ code/Binary.Experimental.Tests/BitArrayConverterTests.cs
@@ -0,0 +1,72 @@
+﻿namespace Mikodev.Binary.Experimental.Tests;
+
+using System;
+using System.Collections;
+using System.Linq;
+using Xunit;
+
+public class BitArrayConverterTests
+{
+    [Fact(DisplayName = "Null Value")]
+    public void NullValue()
+    {
+        var converter = new BitArrayConverter();
+        var buffer = converter.Encode(null);
+        Assert.Empty(buffer);
+        var result = converter.Decode(Array.Empty<byte>());
+        Assert.Null(result);
+    }
+
+    [Theory(DisplayName = "Empty Value")]
+    [InlineData(new byte[] { 0 })]
+    [InlineData(new byte[] { 0x80, 0, 0, 0 })]
+    public void EmptyValue(byte[] header)
+    {
+        var converter = new BitArrayConverter();
+        var source = new BitArray(0);
+        var buffer = converter.Encode(source);
+        Assert.Equal(new byte[] { 0 }, buffer);
+        var result = converter.Decode(header);
+        Assert.NotNull(result);
+        Assert.Equal(0, result.Length);
+    }
+
+    [Theory(DisplayName = "Decode Invalid Bytes")]
+    [InlineData(new byte[] { 2 })]
+    [InlineData(new byte[] { 7 })]
+    [InlineData(new byte[] { 0x80, 0, 0, 1 })]
+    [InlineData(new byte[] { 0x80, 33, 0, 0 })]
+    public void InvalidBytes(byte[] bytes)
+    {
+        var converter = new BitArrayConverter();
+        var error = Assert.Throws<ArgumentException>(() => converter.Decode(bytes));
+        Assert.Null(error.ParamName);
+        Assert.Equal($"Invalid padding byte(s), type: {typeof(BitArray)}", error.Message);
+    }
+
+    [Fact(DisplayName = "Encode Decode Random Data (bit length: 0..128, loop count: 16)")]
+    public void EncodeDecodeRandomData()
+    {
+        const int LoopCount = 16;
+        const int BitLength = 128;
+        var converter = new BitArrayConverter();
+        for (var loop = 0; loop < LoopCount; loop++)
+        {
+            var bytes = new byte[BitLength / 8];
+            Random.Shared.NextBytes(bytes);
+            for (var i = 0; i < BitLength; i++)
+            {
+                var source = new BitArray(bytes) { Length = i };
+                var buffer = converter.Encode(source);
+                var intent = new ReadOnlySpan<byte>(buffer);
+                var header = Converter.Decode(ref intent);
+                Assert.Equal((i % 8) is 0 ? 0 : (8 - (i & 7)), header);
+                Assert.Equal((i + 7) / 8, intent.Length);
+                var result = converter.Decode(buffer);
+                Assert.NotNull(result);
+                Assert.Equal(i, result.Length);
+                Assert.Equal(source.Cast<bool>(), result.Cast<bool>());
+            }
+        }
+    }
+}
---
+++ code/Binary.Experimental/BitArrayConverter.cs
@@ -0,0 +1,49 @@
+﻿namespace Mikodev.Binary.Experimental;
+
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+public sealed class BitArrayConverter : Converter<BitArray?>
+{
+    /* BitArray Converter
+     * Layout: padding bit count for last byte | bytes
+     */
+
+    public override void Encode(ref Allocator allocator, BitArray? item)
+    {
+        if (item is null)
+            return;
+        var bitLength = item.Length;
+        var padding = (-bitLength) & 7;
+        Converter.Encode(ref allocator, padding);
+        if (bitLength is 0)
+            return;
+        var byteLength = (bitLength + 7) / 8;
+        var bytes = new byte[byteLength];
+        item.CopyTo(bytes, 0);
+        Allocator.Append(ref allocator, bytes);
+    }
+
+    public override BitArray? Decode(in ReadOnlySpan<byte> span)
+    {
+        [DoesNotReturn]
+        [DebuggerStepThrough]
+        static BitArray Except() => throw new ArgumentException($"Invalid padding byte(s), type: {typeof(BitArray)}");
+
+        var limits = span.Length;
+        if (limits is 0)
+            return null;
+        var intent = span;
+        var header = (uint)Converter.Decode(ref intent);
+        if (intent.IsEmpty)
+            return header is 0 ? new BitArray(0) : Except();
+        if (header >= 8)
+            return Except();
+        var buffer = intent.ToArray();
+        var result = new BitArray(buffer);
+        result.Length -= (int)header;
+        return result;
+    }
+}
