05cc3a645e9d12dd4fb442af0bdb0a746554308e
miko <stdarg@outlook.com>
2023-03-28 15:10:03 +0800

    Add test data for validation

--- code/Binary.SourceGeneration.Tests/DiagnosticTests/RequirePublicGetterTests.cs
+++ code/Binary.SourceGeneration.Tests/DiagnosticTests/RequirePublicGetterTests.cs
@@ -44,8 +44,26 @@ public class RequirePublicGetterTests
                 public string Name { set => name = value; }
             }
             """;
+        var c =
+            """
+            namespace Tests;
+
+            using Mikodev.Binary.Attributes;
+
+            [SourceGeneratorContext]
+            [SourceGeneratorInclude<Delta>]
+            partial class DeltaSourceGeneratorContext { }
+
+            [NamedObject]
+            public class Delta
+            {
+                [NamedKey("internal")]
+                public string Tags { internal get; set; }
+            }
+            """;
         yield return new object[] { a, "Id", "Alpha" };
         yield return new object[] { b, "Name", "Bravo" };
+        yield return new object[] { c, "Tags", "Delta" };
     }
 
     [Theory(DisplayName = "Require Public Getter")]
