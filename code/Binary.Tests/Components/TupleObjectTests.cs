a631db7f736fda2ce069343c82dbd373f12acf2a
miko <stdarg@outlook.com>
2023-05-24 09:46:23 +0800

    Fix tuple object module and unit tests

--- code/Binary.Tests/Components/TupleObjectTests.cs
+++ code/Binary.Tests/Components/TupleObjectTests.cs
@@ -8,6 +8,27 @@ using Xunit;
 
 public class TupleObjectTests
 {
+    private sealed class HideConverter : IConverter
+    {
+        public int Length => throw new NotSupportedException();
+
+        public void Encode(ref Allocator allocator, object? item) => throw new NotSupportedException();
+
+        public void EncodeAuto(ref Allocator allocator, object? item) => throw new NotSupportedException();
+
+        public void EncodeWithLengthPrefix(ref Allocator allocator, object? item) => throw new NotSupportedException();
+
+        public byte[] Encode(object? item) => throw new NotSupportedException();
+
+        public object? Decode(in ReadOnlySpan<byte> span) => throw new NotSupportedException();
+
+        public object? DecodeAuto(ref ReadOnlySpan<byte> span) => throw new NotSupportedException();
+
+        public object? DecodeWithLengthPrefix(ref ReadOnlySpan<byte> span) => throw new NotSupportedException();
+
+        public object? Decode(byte[]? buffer) => throw new NotSupportedException();
+    }
+
     private sealed class FakeConverter<T> : Converter<T>
     {
         public FakeConverter(int length) : base(length) { }
@@ -32,5 +53,35 @@ public class TupleObjectTests
         var converter = new FakeConverter<int>(0x4000_0000);
         var error = Assert.Throws<OverflowException>(() => TupleObject.GetTupleObjectLength(new[] { converter, converter }));
         Assert.Equal(new OverflowException().Message, error.Message);
+    }
+
+    [Fact(DisplayName = "Sequence Null Test")]
+    public void SequenceNullTest()
+    {
+        var methodInfo = ReflectionExtensions.GetMethodNotNull(typeof(TupleObject), "GetTupleObjectLength", BindingFlags.Static | BindingFlags.Public);
+        var error = Assert.Throws<ArgumentException>(() => TupleObject.GetTupleObjectLength(new IConverter[] { null! }));
+        var parameters = methodInfo.GetParameters();
+        Assert.Equal(parameters[0].Name, error.ParamName);
+        Assert.StartsWith("Sequence contains null or invalid element.", error.Message);
+    }
+
+    [Fact(DisplayName = "Sequence Invalid Test")]
+    public void SequenceInvalidTest()
+    {
+        var methodInfo = ReflectionExtensions.GetMethodNotNull(typeof(TupleObject), "GetTupleObjectLength", BindingFlags.Static | BindingFlags.Public);
+        var error = Assert.Throws<ArgumentException>(() => TupleObject.GetTupleObjectLength(new IConverter[] { new HideConverter() }));
+        var parameters = methodInfo.GetParameters();
+        Assert.Equal(parameters[0].Name, error.ParamName);
+        Assert.StartsWith("Sequence contains null or invalid element.", error.Message);
+    }
+
+    [Fact(DisplayName = "Sequence Empty Test")]
+    public void SequenceEmptyTest()
+    {
+        var methodInfo = ReflectionExtensions.GetMethodNotNull(typeof(TupleObject), "GetTupleObjectLength", BindingFlags.Static | BindingFlags.Public);
+        var error = Assert.Throws<ArgumentException>(() => TupleObject.GetTupleObjectLength(Array.Empty<IConverter>()));
+        var parameters = methodInfo.GetParameters();
+        Assert.Equal(parameters[0].Name, error.ParamName);
+        Assert.StartsWith("Sequence contains no element.", error.Message);
     }
 }
--- code/Binary/Components/TupleObject.cs
+++ code/Binary/Components/TupleObject.cs
@@ -1,5 +1,6 @@
 ﻿namespace Mikodev.Binary.Components;
 
+using Mikodev.Binary.Internal.Metadata;
 using System;
 using System.Collections.Generic;
 
@@ -8,14 +9,21 @@ public static class TupleObject
     public static int GetTupleObjectLength(IEnumerable<IConverter> converters)
     {
         ArgumentNullException.ThrowIfNull(converters);
+        var invoke = false;
         var result = 0;
         foreach (var i in converters)
         {
+            invoke = true;
+            if (i is not IConverterMetadata)
+                throw new ArgumentException($"Sequence contains null or invalid element.", nameof(converters));
             var length = i.Length;
             if (length is 0)
                 return 0;
             checked { result += length; }
         }
+
+        if (invoke is false)
+            throw new ArgumentException("Sequence contains no element.", nameof(converters));
         return result;
     }
 }
