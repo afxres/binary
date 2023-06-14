42a553ce4fc04a75c0bd2b5cdee60ff6b27a5d92
miko <stdarg@outlook.com>
2023-06-14 10:47:27 +0800

    Use immutable array marshal

--- code/Binary/Internal.SpanLike.Builders/ImmutableArrayBuilder.cs
+++ code/Binary/Internal.SpanLike.Builders/ImmutableArrayBuilder.cs
@@ -3,17 +3,14 @@
 using Mikodev.Binary.Internal.SpanLike.Contexts;
 using System;
 using System.Collections.Immutable;
-using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 internal readonly struct ImmutableArrayBuilder<E> : ISpanLikeBuilder<ImmutableArray<E>, E>
 {
     public static ImmutableArray<E> Invoke(E[] array, int count)
     {
-        // TODO: use 'ImmutableCollectionsMarshal.AsImmutableArray'
-        static ImmutableArray<E> Create(E[] array) => Unsafe.As<E[], ImmutableArray<E>>(ref array);
-
         if (array.Length != count)
             Array.Resize(ref array, count);
-        return Create(array);
+        return ImmutableCollectionsMarshal.AsImmutableArray(array);
     }
 }
