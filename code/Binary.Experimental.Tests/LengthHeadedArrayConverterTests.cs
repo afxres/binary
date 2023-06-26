f3bc59459605fc59416410920da86440bb25db5a
miko <stdarg@outlook.com>
2023-06-26 22:46:06 +0800

    Add experimental length headed array converter and unit tests

---
+++ code/Binary.Experimental.Tests/LengthHeadedArrayConverterTests.cs
@@ -0,0 +1,66 @@
+﻿namespace Mikodev.Binary.Experimental.Tests;
+
+using System;
+using System.Linq;
+using Xunit;
+
+public class LengthHeadedArrayConverterTests
+{
+    [Theory(DisplayName = "Encode Decode Test")]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(128)]
+    [InlineData(1027)]
+    [InlineData(65537)]
+    public void EncodeDecodeTest(int length)
+    {
+        var source = Enumerable.Range(0, length).ToArray();
+        var generator = Generator.CreateAot();
+        var converter = new LengthHeadedArrayConverter<int>(generator.GetConverter<int>());
+        var buffer = converter.Encode(source);
+        var result = converter.Decode(buffer);
+        Assert.Equal(source, result);
+    }
+
+    [Theory(DisplayName = "Length Header Test")]
+    [InlineData(0, 1)]
+    [InlineData(127, 1)]
+    [InlineData(128, 4)]
+    [InlineData(65537, 4)]
+    public void LengthHeaderTest(int length, int headerByteLength)
+    {
+        var source = Enumerable.Range(0, length).ToArray();
+        var generator = Generator.CreateAot();
+        var converter = new LengthHeadedArrayConverter<int>(generator.GetConverter<int>());
+        var buffer = converter.Encode(source);
+        var actual = Converter.Decode(buffer, out var bytesRead);
+        Assert.Equal(length, actual);
+        Assert.Equal(headerByteLength, bytesRead);
+    }
+
+    [Fact(DisplayName = "Null Array Test")]
+    public void NullArrayTest()
+    {
+        var generator = Generator.CreateAot();
+        var converter = new LengthHeadedArrayConverter<int>(generator.GetConverter<int>());
+        var buffer = converter.Encode(null);
+        Assert.Empty(buffer);
+        var result = converter.Decode(Array.Empty<byte>());
+        Assert.Null(result);
+    }
+
+    [Fact(DisplayName = "Empty Array Test")]
+    public void EmptyArrayTest()
+    {
+        var generator = Generator.CreateAot();
+        var converter = new LengthHeadedArrayConverter<int>(generator.GetConverter<int>());
+        var buffer = converter.Encode(Array.Empty<int>());
+        Assert.Equal(new byte[] { 0 }, buffer);
+        var a = converter.Decode(new byte[] { 0 });
+        var b = converter.Decode(new byte[] { 0x80, 0, 0, 0 });
+        Assert.NotNull(a);
+        Assert.NotNull(b);
+        Assert.Empty(a);
+        Assert.Empty(b);
+    }
+}
---
+++ code/Binary.Experimental/LengthHeadedArrayConverter.cs
@@ -0,0 +1,36 @@
+﻿namespace Mikodev.Binary.Experimental;
+
+using System;
+
+public sealed class LengthHeadedArrayConverter<E>(Converter<E> converter) : Converter<E[]?>
+{
+    /* Alternative Array Converter With Extra Header For Array Length
+     * Layout: array length | array data
+     */
+
+    private readonly Converter<E> converter = converter;
+
+    public override void Encode(ref Allocator allocator, E[]? item)
+    {
+        if (item is null)
+            return;
+        Converter.Encode(ref allocator, item.Length);
+        var converter = this.converter;
+        for (var i = 0; i < item.Length; i++)
+            converter.EncodeAuto(ref allocator, item[i]);
+        return;
+    }
+
+    public override E[]? Decode(in ReadOnlySpan<byte> span)
+    {
+        if (span.Length is 0)
+            return null;
+        var body = span;
+        var length = Converter.Decode(ref body);
+        var result = new E[length];
+        var converter = this.converter;
+        for (var i = 0; i < result.Length; i++)
+            result[i] = converter.DecodeAuto(ref body);
+        return result;
+    }
+}
