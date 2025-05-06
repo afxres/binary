0509dd5bb16d64bdd47e88fff83dd1f234f8e970
miko <stdarg@outlook.com>
2025-05-06 23:06:10 +0800

    Fix fsharp list decode methods

--- code/Binary.FSharp/Creators/ListConverter.fs
+++ code/Binary.FSharp/Creators/ListConverter.fs
@@ -1,80 +1,8 @@
 ﻿namespace Mikodev.Binary.Creators
 
 open Mikodev.Binary
+open Mikodev.Binary.Internal
 open System
-open System.Diagnostics
-open System.Runtime.CompilerServices
-open System.Runtime.InteropServices
-
-module internal ListMethods =
-    open System.Collections.Generic
-    [<Literal>]
-    let MaxLevels = 32
-
-    [<Literal>]
-    let NewLength = 64
-
-    [<Struct>]
-    [<NoEquality>]
-    [<NoComparison>]
-    [<InlineArray(MaxLevels)>]
-    type InlineBuffer<'T> =
-        [<DefaultValue(false)>]
-        val mutable private Item: 'T
-
-    [<DebuggerStepThrough>]
-    let ExceptConstant (length: int, t: Type) : unit =
-        raise (ArgumentException $"Not enough bytes for collection element, byte length: {length}, element type: {t}")
-
-    let GetListFromSpan<'E> (span: Span<'E>) =
-        let mutable result = []
-        for i = span.Length - 1 downto 0 do
-            result <- span[i] :: result
-        result
-
-    let GetPartialList<'E> (converter: Converter<'E>, span: byref<ReadOnlySpan<byte>>) =
-        let mutable buffer = new InlineBuffer<'E>()
-        let target = MemoryMarshal.CreateSpan(&Unsafe.As<InlineBuffer<'E>, 'E>(&buffer), MaxLevels)
-        let mutable cursor = 0
-        while cursor <> MaxLevels && span.Length <> 0 do
-            target[cursor] <- converter.DecodeAuto &span
-            cursor <- cursor + 1
-        let source = target.Slice(0, cursor)
-        if span.Length = 0 then
-            Choice1Of2(GetListFromSpan source)
-        else
-            let result = ResizeArray<'E> NewLength
-            CollectionExtensions.AddRange(result, source)
-            Choice2Of2 result
-
-    let GetListWithConstantConverter<'E> (converter: Converter<'E>, span: byref<ReadOnlySpan<byte>>) =
-        let itemLength = converter.Length
-        let spanLength = span.Length
-        if (spanLength % itemLength) <> 0 then
-            ExceptConstant(spanLength, typeof<'E>)
-        let mutable result = []
-        let mutable i = spanLength - itemLength
-        while i >= 0 do
-            let data = span.Slice(i, itemLength)
-            let head = converter.Decode &data
-            result <- head :: result
-            i <- i - itemLength
-        result
-
-    let GetListWithVariableConverter<'E> (converter: Converter<'E>, span: byref<ReadOnlySpan<byte>>) =
-        match GetPartialList(converter, &span) with
-        | Choice1Of2 result -> result
-        | Choice2Of2 source ->
-            assert (source.Count = MaxLevels)
-            assert (source.Capacity = NewLength)
-            while span.Length <> 0 do
-                source.Add(converter.DecodeAuto(&span))
-            GetListFromSpan(CollectionsMarshal.AsSpan source)
-
-    let GetList (converter: Converter<'E>, span: byref<ReadOnlySpan<byte>>) =
-        if span.Length = 0 then []
-        elif converter.Length <> 0 then GetListWithConstantConverter(converter, &span)
-        else GetListWithVariableConverter(converter, &span)
 
 [<CompiledName("FSharpListConverter`1")>]
 type internal ListConverter<'T>(converter: Converter<'T>) =
@@ -87,6 +15,4 @@ type internal ListConverter<'T>(converter: Converter<'T>) =
                 converter.EncodeAuto(&allocator, i)
         ()
 
-    override __.Decode(span: inref<ReadOnlySpan<byte>>) : 'T list =
-        let mutable body = span
-        ListMethods.GetList(converter, &body)
+    override __.Decode(span: inref<ReadOnlySpan<byte>>) : 'T list = ObjectHelper.GetList(converter, span)
---
+++ code/Binary.FSharp/Internal/ObjectHelper.fs
@@ -0,0 +1,79 @@
+module internal Mikodev.Binary.Internal.ObjectHelper
+
+open Mikodev.Binary
+open System
+open System.Collections.Generic
+open System.Diagnostics
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<Literal>]
+let MaxLevels = 32
+
+[<Literal>]
+let NewLength = 64
+
+[<Struct>]
+[<NoEquality>]
+[<NoComparison>]
+[<InlineArray(MaxLevels)>]
+type InlineBuffer<'T> =
+    [<DefaultValue(false)>]
+    val mutable private Item: 'T
+
+[<DebuggerStepThrough>]
+let ExceptConstant (length: int, t: Type) : unit =
+    raise (ArgumentException $"Not enough bytes for collection element, byte length: {length}, element type: {t}")
+
+let GetListFromSpan<'E> (span: Span<'E>) =
+    let mutable result = []
+    let mutable cursor = span.Length - 1
+    while cursor >= 0 do
+        result <- span[cursor] :: result
+        cursor <- cursor - 1
+    result
+
+let GetPartialList<'E> (converter: Converter<'E>, span: byref<ReadOnlySpan<byte>>) =
+    let mutable buffer = new InlineBuffer<'E>()
+    let target = MemoryMarshal.CreateSpan(&Unsafe.As<InlineBuffer<'E>, 'E>(&buffer), MaxLevels)
+    let mutable cursor = 0
+    while cursor <> MaxLevels && span.Length <> 0 do
+        target[cursor] <- converter.DecodeAuto &span
+        cursor <- cursor + 1
+    let source = target.Slice(0, cursor)
+    if span.Length = 0 then
+        Choice1Of2(GetListFromSpan source)
+    else
+        let result = ResizeArray<'E> NewLength
+        CollectionExtensions.AddRange(result, source)
+        Choice2Of2 result
+
+let GetListWithConstantConverter<'E> (converter: Converter<'E>, span: ReadOnlySpan<byte>) =
+    let itemLength = converter.Length
+    let spanLength = span.Length
+    if (spanLength % itemLength) <> 0 then
+        ExceptConstant(spanLength, typeof<'E>)
+    let mutable result = []
+    let mutable cursor = spanLength - itemLength
+    while cursor >= 0 do
+        let data = span.Slice(cursor, itemLength)
+        let head = converter.Decode &data
+        result <- head :: result
+        cursor <- cursor - itemLength
+    result
+
+let GetListWithVariableConverter<'E> (converter: Converter<'E>, span: ReadOnlySpan<byte>) =
+    let mutable body = span
+    match GetPartialList(converter, &body) with
+    | Choice1Of2 result -> result
+    | Choice2Of2 source ->
+        assert (source.Count = MaxLevels)
+        assert (source.Capacity = NewLength)
+        while body.Length <> 0 do
+            source.Add(converter.DecodeAuto(&body))
+        GetListFromSpan(CollectionsMarshal.AsSpan source)
+
+let GetList (converter: Converter<'E>, span: ReadOnlySpan<byte>) =
+    if span.Length = 0 then []
+    elif converter.Length <> 0 then GetListWithConstantConverter(converter, span)
+    else GetListWithVariableConverter(converter, span)
